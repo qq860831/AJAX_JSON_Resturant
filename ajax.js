1ce1e434ee880c69e4f5cdc0b0b105874b4ee953
HWChia <66512878+xmas21@users.noreply.github.com>
2020-08-02 12:31:57 +0800

    123

--- ajax.js
+++ ajax.js
@@ -5,6 +5,14 @@ new Vue({
 
     },
     mounted() {
-        
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET"," https://datacenter.taichung.gov.tw/swagger/OpenData/72b2e32b-74e4-4000-b920-7457c54565be")
+        xhr.send();
+
+        xhr.onload = () => {
+            var json = JSON.parse(xhr.responseText);
+            this.datas = json;
+            console.log(json);
+        }
     },
 })
